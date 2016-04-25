2a6cdea01ea7928fdb45609ca3c343334703788c
Chris Tsoi <cjt6@aber.ac.uk>
2016-04-25 18:24:10 +0100

    Added basic website content
    Signed-off-by: Chris Tsoi <cjt6@aber.ac.uk>

---
+++ Weather_Station/Website/weather.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<?php
+	require ("db_connect.php");
+?>
+<html lang="en">
+
+<head> <!-- HTML Header that contains information on the page -->
+	<meta charset="UTF-8"/>
+	<link rel="stylesheet" type="text/css" href="styling.css">
+	<title>Weather Weather Station</title>
+</head>
+
+<body> <!-- HTML Body that contains site content -->
+	<section>
+		<h1 class="title">Aberystywyth Weather Station</h1>
+
+	</section>
+	
+	<!-- Create temperature graph -->
+
+</body>
+
+</html>
